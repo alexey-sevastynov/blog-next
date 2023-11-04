eb32951e4983f87e8d3d090681d22776d0cd402e
Alexey Sevastynov <alexseva94@gmail.com>
2023-11-04 10:54:13 +0200

    connect mongoDB & auth-next google

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,53 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import connect from "@/utils/db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+
+      //@ts-ignore
+      async authorize(
+        credentials: {
+          email: string;
+          password: string;
+        },
+        request
+      ) {
+        await connect();
+        try {
+          const user = await User.findOne({ email: credentials.email });
+          if (user) {
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+            if (isPasswordCorrect) {
+              return user;
+            } else {
+              throw new Error("Wrong credentials");
+            }
+          } else {
+            throw new Error("User not found!");
+          }
+        } catch (error: any) {
+          throw new Error(error);
+        }
+      },
+    }),
+  ],
+  pages: {
+    error: "/",
+  },
+});
+
+export { handler as GET, handler as POST };
--- src/app/components/AuthWindow/AuthWindow.tsx
+++ src/app/components/AuthWindow/AuthWindow.tsx
@@ -1,15 +1,41 @@
+"use client";
+
 import Button from "@/components/Button/Button";
 import styles from "./auth.module.scss";
 import { COLORS } from "../../../constants/colors";
 import Or from "../../../components/Or/Or";
 import Link from "next/link";
+import { signIn, useSession } from "next-auth/react";
+import { FormEvent } from "react";
+import { useRouter } from "next/navigation";
 
 const AuthWindow = () => {
+  const session = useSession();
+  // const router = useRouter();
+
+  console.log(session);
+
+  if (session.status == "loading") {
+    return <p>Loading</p>;
+  }
+
+  // if (session.status == "authenticated") {
+  //   router?.push("/");
+  // }
+
   return (
     <section className={styles.auth}>
       <main className={styles.authWindow}>
         <header className={styles.authRegister}>
-          <Button iconSrc={"/google.png"}>Register via google</Button>
+          <Button
+            iconSrc={"/google.png"}
+            onClick={(e) => {
+              e.preventDefault();
+              signIn("google");
+            }}
+          >
+            Register via google
+          </Button>
           <Or />
           <Link href={"/register"}>
             <Button style={{ fontWeight: "700", background: COLORS.yellow }}>
