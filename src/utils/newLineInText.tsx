6e91e549337bf5cb1b4211a712dfc07ff0103440
Alexey Sevastynov <alexseva94@gmail.com>
2023-11-19 12:16:56 +0200

    removed spaces after the post text. fixed confirm window positioning

--- src/utils/newLineInText.tsx
+++ src/utils/newLineInText.tsx
@@ -1,10 +1,16 @@
 export const newLineInText = (text: string) => {
   const paragraphs = text.split(/\n{2,}|\n{1}(?!\n)/);
+  const totalParagraphs = paragraphs.length;
+
   return paragraphs.map((paragraph, index) => (
     <p key={index}>
       {paragraph}
-      <br />
-      <br />
+      {index !== totalParagraphs - 1 && (
+        <>
+          <br />
+          <br />
+        </>
+      )}
     </p>
   ));
 };
